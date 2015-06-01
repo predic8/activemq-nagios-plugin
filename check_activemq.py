2c2463bdad652fcec989521b92ee37447c620226
Lennart Buhl <git@hackmate.de>
2015-06-01 18:14:32 +0200

    start work on health feature

--- check_activemq.py
+++ check_activemq.py
@@ -24,6 +24,10 @@ def queue_url(args, queue):
 def topic_url(args, topic):
 	return make_url(args, 'org.apache.activemq:type=Broker,brokerName=localhost,destinationType=Topic,destinationName='+topic)
 
+def health_url(args):
+	return make_url(args, 'org.apache.activemq:type=Broker,brokerName=localhost,service=Health')
+
+
 
 def queuesize(args):
 
@@ -31,14 +35,13 @@ def queuesize(args):
 		def evaluate(self, metric, resource):
 			if metric.value < 0:
 				return self.result_cls(np.Critical, "FAIL", metric)
-			if not self.critical.match(metric.value):
-				return self.result_cls(np.Critical, self.critical.violation, metric)
-			elif not self.warning.match(metric.value):
-				return self.result_cls(np.Warn, self.warning.violation, metric)
-			else:
-				return self.result_cls(np.Ok, None, metric)
+			return super(ActiveMqScalarContext, self).evaluate(metric, resource)
+		def describe(self, metric):
+			if metric.value < 0:
+				return metric.name
+			return super(ActiveMqScalarContext, self).describe(metric)
 
-	class ActiveMqQueueSize(np.Resource): # Check ALL queues of the server
+	class ActiveMqQueueSize(np.Resource):
 		def __init__(self, pattern=None):
 			self.pattern = pattern
 		def probe(self):
@@ -62,11 +65,11 @@ def queuesize(args):
 				yield np.Metric('Decoding Json FAILED.', -1, context='size')
 			except KeyError as e:
 				#_log.error('Loading Queue(s) FAILED. ' + str(e))
-				yield np.Metric('Loading Queue(s) FAILED.', -1, context='size')
+				yield np.Metric('Getting Queue(s) FAILED.', -1, context='size')
 
 	if args.queue:
 		check = np.Check(
-				ActiveMqQueueSize(args.queue), ## check ONE queue
+				ActiveMqQueueSize(args.queue), ## check ONE queue (or glob)
 				ActiveMqScalarContext('size', args.warn, args.crit)
 			)
 		check.main()
@@ -77,14 +80,50 @@ def queuesize(args):
 			)
 		check.main()
 
+
+
 def health(args):
-	#global username, password, host, port
-	pass
+
+	class ActiveMqHealthContext(np.Context):
+		def evaluate(self, metric, resource):
+			if metric.value == "Good":
+				return self.result_cls(np.Ok, metric=metric)
+			else:
+				return self.result_cls(np.Warning, metric=metric)
+		def describe(self, metric):
+			if not self.fmt_metric:
+				return
+			try:
+				return self.fmt_metric(metric, self)
+			except TypeError:
+				return self.fmt_metric.format(
+					name=metric.name, value=metric.value, uom=metric.uom,
+					valueunit=metric.valueunit, min=metric.min, max=metric.max)
+
+	class ActiveMqHealth(np.Resource):
+		def probe(self):
+			try:
+				jsn = urllib.urlopen(health_url(args))
+				resp = json.loads(jsn.read())
+				status = resp['value']['CurrentStatus']
+				return np.Metric('CurrentStatus', status, context='health')
+			except IOError as e:
+				return np.Metric('Network fetching FAILED.', -1, context='health')
+			except ValueError as e:
+				return np.Metric('Decoding Json FAILED.', -1, context='health')
+			except KeyError as e:
+				return np.Metric('Getting Values FAILED.', -1, context='health')
+
+	check = np.Check(
+			ActiveMqHealth(), ## check ONE queue
+			ActiveMqScalarContext('health', args.warn, args.crit)
+		)
+	check.main()
+
+
 
 def subscriber(args):
-	#global username, password, host, port
 	pass
-
 
 
 
@@ -113,10 +152,10 @@ def main():
 			help='Critical if Queue Size is greater. (default: %(default)s)')
 	parser_queuesize.add_argument('queue', nargs='?',
 			help='''Name of the Queue that will be checked.
-			        If left empty, all Queues will be checked.
-			        This also can be a Unix shell-style Wildcard
-			        (much less powerful than a RegEx)
-			        where * and ? can be used.''')
+					If left empty, all Queues will be checked.
+					This also can be a Unix shell-style Wildcard
+					(much less powerful than a RegEx)
+					where * and ? can be used.''')
 	parser_queuesize.set_defaults(func=queuesize)
 
 	# Sub-Parser for health
