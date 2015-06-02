f621ca2a959cfd267c1bf6add507c96dbe89c1e1
Lennart Buhl <git@hackmate.de>
2015-06-02 20:15:43 +0200

    Implemented subscriber
    More argument documentation

--- check_activemq.py
+++ check_activemq.py
@@ -12,36 +12,40 @@ import nagiosplugin as np
 
 #_log = logging.getLogger('nagiosplugin')
 
+PREFIX = 'org.apache.activemq:'
+
 def make_url(args, dest):
 	return ('http://'+args.user+':'+args.pwd
 			+'@'+args.host+':'+str(args.port)
 			+'/'+args.jolokia+'/'+dest)
 
 def query_url(args):
-	return make_url(args, 'org.apache.activemq:type=Broker,brokerName=localhost')
+	return make_url(args, PREFIX + 'type=Broker,brokerName=localhost')
 
 def queue_url(args, queue):
-	return make_url(args, 'org.apache.activemq:type=Broker,brokerName=localhost,destinationType=Queue,destinationName='+queue)
+	return make_url(args, PREFIX + 'type=Broker,brokerName=localhost,destinationType=Queue,destinationName='+queue)
 
 def topic_url(args, topic):
-	return make_url(args, 'org.apache.activemq:type=Broker,brokerName=localhost,destinationType=Topic,destinationName='+topic)
+	return make_url(args, PREFIX + 'type=Broker,brokerName=localhost,destinationType=Topic,destinationName='+topic)
 
 def health_url(args):
-	return make_url(args, 'org.apache.activemq:type=Broker,brokerName=localhost,service=Health')
+	return make_url(args, PREFIX + 'type=Broker,brokerName=localhost,service=Health')
+
+
 
 
 
 def queuesize(args):
 
-	class ActiveMqScalarContext(np.ScalarContext):
+	class ActiveMqQueueSizeScalarContext(np.ScalarContext):
 		def evaluate(self, metric, resource):
 			if metric.value < 0:
 				return self.result_cls(np.Critical, "FAIL", metric)
-			return super(ActiveMqScalarContext, self).evaluate(metric, resource)
+			return super(ActiveMqQueueSizeScalarContext, self).evaluate(metric, resource)
 		def describe(self, metric):
 			if metric.value < 0:
 				return metric.name
-			return super(ActiveMqScalarContext, self).describe(metric)
+			return super(ActiveMqQueueSizeScalarContext, self).describe(metric)
 
 	class ActiveMqQueueSize(np.Resource):
 		def __init__(self, pattern=None):
@@ -69,28 +73,31 @@ def queuesize(args):
 				#_log.error('Loading Queue(s) FAILED. ' + str(e))
 				yield np.Metric('Getting Queue(s) FAILED.', -1, context='size')
 
-	class ActiveMqSummary(np.Summary):
+	class ActiveMqQueueSizeSummary(np.Summary):
 		def ok(self, results):
 			if len(results) > 1:
 				lenQ = str(len(results))
 				maxQ = str(max(results, key=lambda r: r.metric.value).metric.value)
 				return 'Checked ' + lenQ + ' queues with a maximum length of ' + maxQ
 			else:
-				return super(ActiveMqSummary, self).ok(results)
+				return super(ActiveMqQueueSizeSummary, self).ok(results)
 
 	if args.queue:
 		check = np.Check(
 				ActiveMqQueueSize(args.queue), ## check ONE queue (or glob)
 				ActiveMqScalarContext('size', args.warn, args.crit),
-				ActiveMqSummary()
+				ActiveMqQueueSizeSummary()
 			)
 		check.main()
 	else:
 		check = np.Check(
 				ActiveMqQueueSize(), # check ALL queues
-				ActiveMqScalarContext('size', args.warn, args.crit)
+				ActiveMqQueueSizeScalarContext('size', args.warn, args.crit),
+				ActiveMqQueueSizeSummary()
 			)
 		check.main()
+
+
 
 
 
@@ -103,7 +110,7 @@ def health(args):
 			else:
 				return self.result_cls(np.Warn, metric=metric)
 		def describe(self, metric):
-			return metric.value
+			return str(metric.value)
 
 	class ActiveMqHealth(np.Resource):
 		def probe(self):
@@ -127,8 +134,63 @@ def health(args):
 
 
 
+
+
 def subscriber(args):
-	pass
+
+	class ActiveMqSubscriberContext(np.Context):
+
+		def evaluate(self, metric, resource):
+			if metric.value == None:
+				return self.result_cls(np.Critical, metric=metric)
+			else:
+				if metric.value:
+					return self.result_cls(np.Ok, metric=metric)
+				else:
+					return self.result_cls(np.Warn, metric=metric)
+		def describe(self, metric):
+			if metric.value == None:
+				return 'Topic ' + args.topic + ' DOES NOT EXIST'
+			return ('Client ' + args.clientId + ' is'
+				    +(''if metric.value else ' NOT a')
+			        +' subscriber of Topic ' + args.topic)
+
+	class ActiveMqSubscriber(np.Resource):
+		def probe(self):
+			try:
+				jsn = urllib.urlopen(topic_url(args, args.topic)) # get a Topic
+				resp = json.loads(jsn.read()) # parse JSON
+
+				if resp['status'] != 200: # None -> Topic doesn't exist
+					return np.Metric('subscription', None, context='subscriber')
+
+				subs = resp['value']['Subscriptions'] # Subscriptions for Topic
+
+				def client_is_subscriber(clientId, subscription):
+					vals = subscription['objectName'].split(',')
+					correct_clientId = (lambda v:
+										v.startswith('clientId=')
+										and v.endswith('='+clientId))
+					return any(map(correct_clientId, vals))
+
+				# check if clientId is among the subscribers
+				is_sub = any(map(lambda s: client_is_subscriber(args.clientId, s), subs))
+				return np.Metric('subscription', is_sub, context='subscriber')
+
+			except IOError as e:
+				return np.Metric('Network fetching FAILED.', -1, context='subscriber')
+			except ValueError as e:
+				return np.Metric('Decoding Json FAILED.', -1, context='subscriber')
+			except KeyError as e:
+				return np.Metric('Getting Values FAILED.', -1, context='subscriber')
+
+	check = np.Check(
+			ActiveMqSubscriber(), ## check ONE queue
+			ActiveMqSubscriberContext('subscriber')
+		)
+	check.main()
+
+
 
 
 
@@ -154,28 +216,37 @@ def main():
 	subparsers = parser.add_subparsers()
 
 	# Sub-Parser for queuesize
-	parser_queuesize = subparsers.add_parser('queuesize', help='Check QueueSize')
+	parser_queuesize = subparsers.add_parser('queuesize',
+			help="""Check QueueSize: This mode checks the queue size of one
+			        or more queues on the ActiveMQ server.
+			        You can specify a queue name to check (even a pattern);
+			        see description of the 'queue' paramter for details.""")
 	parser_queuesize.add_argument('-w', '--warn', metavar='WARN', type=int, default=10,
 			help='Warning if Queue Size is greater. (default: %(default)s)')
 	parser_queuesize.add_argument('-c', '--crit', metavar='CRIT', type=int, default=100,
 			help='Critical if Queue Size is greater. (default: %(default)s)')
 	parser_queuesize.add_argument('queue', nargs='?',
 			help='''Name of the Queue that will be checked.
-					If left empty, all Queues will be checked.
-					This also can be a Unix shell-style Wildcard
-					(much less powerful than a RegEx)
-					where * and ? can be used.''')
+			        If left empty, all Queues will be checked.
+			        This also can be a Unix shell-style Wildcard
+			        (much less powerful than a RegEx)
+			        where * and ? can be used.''')
 	parser_queuesize.set_defaults(func=queuesize)
 
 	# Sub-Parser for health
-	parser_health = subparsers.add_parser('health', help='Check Health')
+	parser_health = subparsers.add_parser('health',
+			help="""Check Health: This mode checks if the current status is 'Good'.""")
 	# no additional arguments necessary
 	parser_health.set_defaults(func=health)
 
 	# Sub-Parser for subscriber
-	parser_subscriber = subparsers.add_parser('subscriber', help='Check Subscriber')
-	parser_subscriber.add_argument('--clientId', required=True)
-	parser_subscriber.add_argument('--topic', required=True)
+	parser_subscriber = subparsers.add_parser('subscriber',
+			help="""Check Subscriber: This mode checks if the given 'clientId'
+			        is a subscriber of the specified 'topic'.""")
+	parser_subscriber.add_argument('--clientId', required=True,
+			help='Client ID of the client that will be checked')
+	parser_subscriber.add_argument('--topic', required=True,
+			help='Name of the Topic that will be checked.')
 	parser_subscriber.set_defaults(func=subscriber)
 
 	# Evaluate Arguments
