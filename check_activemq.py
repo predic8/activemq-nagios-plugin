f9fb336775eb0ea994f66dd288a891342b2041ca
Lennart Buhl <git@hackmate.de>
2015-06-23 18:12:56 +0200

    Project Home in Header

--- check_activemq.py
+++ check_activemq.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 *-*
 
-""" Copyright 2015 predic8 GmbH, www.predic8.com
+"""	Copyright 2015 predic8 GmbH, www.predic8.com
 
 	Licensed under the Apache License, Version 2.0 (the "License");
 	you may not use this file except in compliance with the License.
@@ -13,16 +13,15 @@
 	distributed under the License is distributed on an "AS IS" BASIS,
 	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 	See the License for the specific language governing permissions and
-	limitations under the License.
-"""
+	limitations under the License. """
+
+""" Project Home: https://github.com/predic8/activemq-nagios-plugin """
 
 import urllib
 import json
 import argparse
 import fnmatch
 import nagiosplugin as np
-
-"""Check the size of a given ActiveMQ Queue."""
 
 PLUGIN_VERSION = "0.4.1"
 
